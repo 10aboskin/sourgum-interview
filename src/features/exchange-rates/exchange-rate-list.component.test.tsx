03f7d1680afd261aa1dfdae312322eafb96b05c4
10aboskin <10aboskin@gmail.com>
2024-06-06 14:55:45 -0400

    add text search

--- src/features/exchange-rates/exchange-rate-list.component.test.tsx
+++ src/features/exchange-rates/exchange-rate-list.component.test.tsx
@@ -1,26 +1,37 @@
-import { describe, expect, it } from "vitest";
+import { afterEach, describe, expect, it } from "vitest";
+import { cleanup, render } from "@testing-library/react";
 
 import ExchangeRateList from "./exchange-rate-list.component";
 import QueryClientWrapper from "../../__test__/query-client-wrapper";
 import exchangeRates from "../../__test__/exchange-rates.json";
 import { exchangeRatesPath } from "./exchange-rates.api";
 import nockClient from "../../nock-client";
-import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 const assetNames = exchangeRates.rates.map((item) => item.asset_id_quote);
 const assetRates = exchangeRates.rates.map((item) => item.rate);
 
-describe("ExchangeRateList", () => {
-  it("fetches and displays asset exchange rates with USD", async () => {
-    nockClient
-      .options(exchangeRatesPath)
-      .reply(200)
-      .get(exchangeRatesPath)
-      .reply(200, exchangeRates);
+const setup = async () => {
+  nockClient
+    .options(exchangeRatesPath)
+    .reply(200)
+    .get(exchangeRatesPath)
+    .reply(200, exchangeRates);
 
-    const { findAllByRole } = render(<ExchangeRateList />, {
-      wrapper: QueryClientWrapper,
-    });
+  const utils = render(<ExchangeRateList />, {
+    wrapper: QueryClientWrapper,
+  });
+
+  return utils;
+};
+
+describe("ExchangeRateList", () => {
+  afterEach(() => {
+    cleanup();
+  });
+
+  it("fetches and displays asset exchange rates with USD", async () => {
+    const { findAllByRole } = await setup();
 
     const listItems = await findAllByRole("listitem");
 
@@ -33,5 +44,62 @@ describe("ExchangeRateList", () => {
       item.lastChild?.textContent ? +item.lastChild.textContent : item.lastChild
     );
     expect(listItemPrices).toEqual(assetRates);
+  });
+
+  it("filters the list based on the search input", async () => {
+    const user = userEvent.setup();
+
+    const { findAllByRole, findByRole } = await setup();
+
+    const searchInput = await findByRole("textbox");
+
+    const testListItems = async ({
+      names,
+      rates,
+    }: {
+      names: string[];
+      rates: number[];
+    }) => {
+      const listItems = await findAllByRole("listitem");
+      const listItemNames = listItems.map(
+        (item) => item.firstChild?.textContent
+      );
+      expect(listItemNames).toEqual(names);
+
+      const listItemPrices = listItems.map((item) =>
+        item.lastChild?.textContent
+          ? +item.lastChild.textContent
+          : item.lastChild
+      );
+      expect(listItemPrices).toEqual(rates);
+    };
+
+    // expect to see all items
+    testListItems({ names: assetNames, rates: assetRates });
+    // expect(listItems.length).toEqual(exchangeRates.rates.length);
+
+    // change search value
+    await user.click(searchInput);
+    await user.keyboard("00");
+    // expect to see correct filtered values
+
+    testListItems({
+      names: ["00", "1000SATS"],
+      rates: [13.31439053707501, 3072.441676916485],
+    });
+
+    // change search
+    await user.clear(searchInput);
+    await user.keyboard("1");
+    // expect to see correct filtered values
+    testListItems({
+      names: ["1000SATS", "1EARTH", "1INCH"],
+      rates: [3072.441676916485, 1756.188120859164, 2.078612758834678],
+    });
+
+    //  clear search value
+    await user.clear(searchInput);
+    // expect to see all items
+    testListItems({ names: assetNames, rates: assetRates });
   });
 });
--- src/features/exchange-rates/exchange-rate-list.component.tsx
+++ src/features/exchange-rates/exchange-rate-list.component.tsx
@@ -1,19 +1,49 @@
+import { useMemo, useState } from "react";
+
+import Fuse from "fuse.js";
 import { useUsdExchangeRates } from "./exchange-rates.queries";
 
 export const ExchangeRateList = () => {
   const { data } = useUsdExchangeRates();
 
+  const rates = useMemo(() => data?.rates ?? [], [data?.rates]);
+
+  const [searchValue, setSearchValue] = useState("");
+
+  const fuse = useMemo(
+    () =>
+      new Fuse(rates, {
+        keys: ["asset_id_quote"],
+      }),
+    [rates]
+  );
+
+  const filteredRates = useMemo(
+    () =>
+      searchValue ? fuse.search(searchValue).map(({ item }) => item) : rates,
+    [fuse, rates, searchValue]
+  );
+
   return (
-    <ul>
-      {(data?.rates ?? []).map(({ asset_id_quote, rate }) => {
-        return (
-          <li key={asset_id_quote}>
-            <div id="asset-id">{asset_id_quote}</div>
-            <div id="asset-rate">{rate}</div>
-          </li>
-        );
-      })}
-    </ul>
+    <>
+      <label htmlFor="search-input">Search</label>
+      <input
+        type="text"
+        name="search-input"
+        value={searchValue}
+        onChange={(e) => setSearchValue(e.target.value)}
+      />
+      <ul>
+        {filteredRates.map(({ asset_id_quote, rate }) => {
+          return (
+            <li key={asset_id_quote}>
+              <div id="asset-id">{asset_id_quote}</div>
+              <div id="asset-rate">{rate}</div>
+            </li>
+          );
+        })}
+      </ul>
+    </>
   );
 };
 
